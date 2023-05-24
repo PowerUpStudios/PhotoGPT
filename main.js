dc0841f643a32aa00bdca03059a190b265cd04ad
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-24 17:48:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,20 @@ PGPTConversation.onMessage((msg) => {
       for (let i = 0; i < arr.length; i++) {
         let el = arr[i]
         let q = el.getAttribute("query")
+        let index = el.getAttribute("index")
         if (!q) {
           q = ""
+        }
+        if (!index) {
+          index = 0
+        }else{
+          index = parseInt(index)
         }
         fetch("https://server.powerupstudio.eu/pgpt/ep?name=PhotoGPT&id=GetPhoto&type=" + el.getAttribute("type") + "&query=" + q).then(res => res.json()).then((res) => {
           if (el.getAttribute("type") == "random") {
             el.innerHTML = "<img title='" + res.alt_description + "' src='" + res.urls.raw + "' style='max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
           }else{
-            el.innerHTML = "<img title='" + res.results[0].alt_description + "' src='" + res.results[0].urls.raw + "' style='max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.results[0].user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.results[0].user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
+            el.innerHTML = "<img title='" + res.results[index].alt_description + "' src='" + res.results[index].urls.raw + "' style='max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.results[index].user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.results[index].user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
           }
           if (i + 1 == arr.length) {
             msg.modifyContent(hw.innerHTML)
