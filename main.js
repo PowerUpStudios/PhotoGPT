6c25db5fe7770bb09af5fbed0995db60e69afe30
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-24 18:22:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ PGPTConversation.onMessage((msg) => {
         }
         fetch("https://server.powerupstudio.eu/pgpt/ep?name=PhotoGPT&id=GetPhoto&type=" + el.getAttribute("type") + "&query=" + q).then(res => res.json()).then((res) => {
           if (el.getAttribute("type") == "random") {
-            el.innerHTML = "<img title='" + res.alt_description + "' src='" + res.urls.raw + "' style='max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
+            el.innerHTML = "<img title='" + res.alt_description + "' src='" + res.urls.raw + "' style='border-radius:15px;max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
           }else{
-            el.innerHTML = "<img title='" + res.results[index].alt_description + "' src='" + res.results[index].urls.raw + "' style='max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.results[index].user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.results[index].user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
+            el.innerHTML = "<img title='" + res.results[index].alt_description + "' src='" + res.results[index].urls.raw + "' style='border-radius:15px;max-width:500px;max-height:500px;'></img><br/><i><span>Photo by <a href='https://unsplash.com/@" + res.results[index].user.username + "?utm_source=PhotoGPT&utm_medium=referral'>" + res.results[index].user.name + "</a> on <a href='https://unsplash.com?utm_source=PhotoGPT&utm_medium=referral'>Unsplash</a></span></i>"
           }
           if (i + 1 == arr.length) {
             msg.modifyContent(hw.innerHTML)
