5f32e7acd974a6ddaf7e53490d378d6429022714
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-24 13:26:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ PGPTConversation.onMessage((msg) => {
     msg.contentChanged(null, (c) => {
       const hw = new PGPTConversation.HTMLWorkspace(c, {unescape: true, unescapeInside: "(power-gpt-html|pgpt-extension-register|photogpt)"})
       let arr = hw.getElementsByTagName("photogpt")
-      for (let el of arr) {
+      for (let i = 0; i < arr.length; i++) {
+        let el = arr[i]
         console.log("https://server.powerupstudio.eu/pgpt/ep?name=PhotoGPT&id=GetPhoto&type=" + el.getAttribute("type") + "&query=" + el.getAttribute("query"))
         fetch("https://server.powerupstudio.eu/pgpt/ep?name=PhotoGPT&id=GetPhoto&type=" + el.getAttribute("type") + "&query=" + el.getAttribute("query")).then(res => res.json()).then((res) => {
           if (el.getAttribute("type") == "random") {
             el.innerHTML = "<img src='" + res.urls.raw + "'></img>"
-            console.log("random", el.innerHTML, res.urls.raw)
           }else{
             el.innerHTML = "<img src='" + res[0].urls.raw + "'></img>"
-            console.log("by query", el.innerHTML, res[0].urls.raw)
+          }
+          if (i == arr.length) {
+            msg.modifyContent(hw.innerHTML)
           }
         })
       }
-      console.log(hw.innerHTML, "result")
-      msg.modifyContent(hw.innerHTML)
     })
   }
 }, window.parent.document)
