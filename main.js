9332a2c7a1061a415a065528ef33fcba039c0292
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-24 13:16:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 PGPTConversation.onMessage((msg) => {
-  console.log(msg)
+  if (msg.isGPT) {
+    msg.contentChanged(null, (c) => {
+      const hw = new PGPTConversation.HTMLWorkspace(c, {unescape: true, unescapeInside: "(power-gpt-html|pgpt-extension-register|photogpt)"})
+      let arr = hw.getElementsByTagName("photogpt")
+      for (let el of arr) {
+        fetch("https://server.powerupstudio.eu/pgpt/ep?name=PhotoGPT&id=GetPhoto&type=" + el.getAttribute("type") + "&query=" + el.getAttribute("query")).then(res => res.json()).then((res) => {
+          if (el.getAttribute("type") == "random") {
+            el.innerHTML = "<img src='" + res.urls.raw + "'></img>"
+          }else{
+            el.innerHTML = "<img src='" + res[0].urls.raw + "'></img>"
+          }
+        })
+      }
+      msg.modifyContent(hw.innerHTML)
+    })
+  }
 }, window.parent.document)
